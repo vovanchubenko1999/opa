1c8861dee1ba3e368c671669a33863ce2c79c082
vovanchubenko1999 <62783279+vovanchubenko1999@users.noreply.github.com>
2025-03-15 17:58:38 +0200

    Delete EndClicker directory

--- EndClicker/middleware/__init__.py
+++
@@ -1,5 +0,0 @@
-from aiogram import Dispatcher
-from middleware.throttling_middleware import ThrottlingMiddleware
-
-def setup_middleware(dp: Dispatcher):
-    dp.middleware.setup(ThrottlingMiddleware())
