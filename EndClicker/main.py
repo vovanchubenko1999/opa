3e2997ab08db0dad22c202349ad4621be8a4e363
vovanchubenko1999 <vovan.chubenko1999@gmail.com>
2025-03-15 17:48:31 +0200

    Добавлены файлы после распаковки zip

---
+++ EndClicker/main.py
@@ -0,0 +1,17 @@
+from middleware import setup_middleware
+from handlers import register_handlers
+from config.bot_data import dp
+from aiogram import executor
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def on_startup(_):
+    setup_middleware(dp)
+    logging.info("-_-_- СЛИТ ДЛЯ ENDWAY -_-_-")
+
+register_handlers(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup)
